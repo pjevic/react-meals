7ee30052ef0b0a3d88362d85a6f6058d8655a6b4
pjevic <milanpjevic@icloud.com>
2023-01-19 10:02:01 +0100

    Creating Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Fragment } from "react";
+
+import Header from "./components/Layout/Header/Header";
+
 const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 };
 
