a660774b333c7070258aeb573175450a2d9971fd
pjevic <milanpjevic@icloud.com>
2023-01-24 16:29:03 +0100

    Making Items Removable

--- src/components/Cart/hfhf.js
+++ src/components/Cart/hfhf.js
@@ -1,23 +1,22 @@
 import { useContext } from "react";
 
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 
-import classes from "./Cart.module.css";
-import Modal from "../UI/Modal/Modal";
-import CartItem from "./CartItem";
-
-const Cart = ({ onShowCart }) => {
+const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemAddHandler = (item) => {
-    cartCtx.addItem({ ...item, amount: 1 });
-  };
-
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
   const cartItems = (
@@ -36,14 +35,14 @@ const Cart = ({ onShowCart }) => {
   );
 
   return (
-    <Modal onClick={onShowCart}>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={onShowCart}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
         {hasItems && <button className={classes.button}>Order</button>}
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -25,11 +25,32 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = state.items.concat(action.item);
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
@@ -38,6 +59,7 @@ const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
+
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
